c0153b5e0a3bc60ef6093f412c6d21aa8e839766
karamchand0215 <karamchand.c@gmail.com>
2017-02-03 07:07:25 +0000

    hello

---
+++ a.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+a=10
+if [ $a -gt 9 ]
+then 
+echo "a is grater"
+else
+echo "a is smaller"
+exit 1
+fi
---
+++ test-repo/c.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+c=30
+if [ $c -gt 9 ]
+then 
+echo "c is grater"
+else
+echo "c is smaller"
+exit 1
+fi
